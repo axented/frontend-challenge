362f39ab300a73912013ad8d9dd1839af1166ae4
MiguelED <migueled2695@gmail.com>
2021-03-10 23:41:33 -0600

    Creación de componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BloggersComponent } from './components/bloggers/bloggers.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
+import { CardComponent } from './components/card/card.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BloggersComponent,
+    BuscarComponent,
+    CardComponent,
+    NavbarComponent,
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
