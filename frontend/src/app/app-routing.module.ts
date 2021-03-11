77a60d51785f124c0aae03d1c5bcfac1416dcd48
MiguelED <migueled2695@gmail.com>
2021-03-11 00:05:40 -0600

    Routing  Agregamos las rutas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+/*Components--------------------------------------------------*/
+import { RegisterComponent } from './components/register/register.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BloggersComponent } from './components/bloggers/bloggers.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'register',           component: RegisterComponent },
+    { path: 'list',               component: BloggersComponent },
+    /*Params-----------------------------------------------------*/
+    { path: 'profile/:id',        component: ProfileComponent },
+    { path: 'buscar/:buscarpor',  component: BuscarComponent },
+    /*Exception--------------------------------------------------*/
+    { path: '**', pathMatch: 'full', redirectTo: 'list' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
