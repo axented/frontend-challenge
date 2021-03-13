df8fd2079eb99225f622136cb5289608989afddb
MiguelED <migueled2695@gmail.com>
2021-03-13 00:12:38 -0600

    Agregar y eliminar bloggers desde busqueda

--- frontend/src/app/components/buscar/buscar.component.ts
+++ frontend/src/app/components/buscar/buscar.component.ts
@@ -15,6 +15,7 @@ export class BuscarComponent implements OnInit {
 
     bloggers:    any   = {} ;
     termino:    string = '' ;
+    listFriendsAuthUser    : string[]=[];
 
     constructor(private activatedRoute: ActivatedRoute,
                 private _bloggersService:BloggersService,
@@ -23,10 +24,22 @@ export class BuscarComponent implements OnInit {
     ngOnInit(): void {
         this.activatedRoute.params.subscribe( params => {
             this.termino  = params['buscarpor'];
-            this.bloggers = this._bloggersService.buscarBloggers( params['buscarpor'] );            
+            this.bloggers = this._bloggersService.buscarBloggers( params['buscarpor'] );
+            this.listFriendsAuthUser=this._bloggersService.getArrayFriendsUserAuth('0');
         });
     }
+    
+    isFriend(idBlogger:string){
 
+        for (let index = 0; index < this.listFriendsAuthUser.length; index++) {        
+            if( this.listFriendsAuthUser[index] == idBlogger ){
+                return true;
+            }        
+        }
+
+        return false;
+    }
+    
     vacioObjeto(){
         if( Object.entries(this.bloggers).length === 0 ){
             return true;
